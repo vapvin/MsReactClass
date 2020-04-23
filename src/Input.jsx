40a8be3eb61629a1e58552c7e341ebe8c33bf5cc
vapvin <tpehdj12@daum.net>
2020-04-24 01:11:22 +0900

    Input State

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,24 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 const Input = () => {
-    const [input, setInput] = useState("");
-    
+    const [inputs, setInputs] = useState({
+        phone: "",
+        email: ""
+    });
+    const phoneInput = useRef();
+    const {phone, email} = inputs;
     const change = e => {
-        setInput(e.target.value);
+        const {name, value} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        })
     }
-    
+
     const reset = () => {
-        setInput("");
+        setInputs({
+            phone: "",
+            email: ""
+        });
+        phoneInput.current.focus();
     }
 
     return (
         <div>
-            <input type="text" onChange={change} value={input} />
+            <input name="phone" placeholder="폰번" onChange={change} value={phone} ref={phoneInput} />
+            <input name="email" placeholder="이메일" onChange={change} value={email} />
             <button onClick={reset}>초기화</button>
-            <p>
-                <b>값:</b>
-                <span>{input}</span>
-            </p>
+            <ul>
+                <li>폰번호: {phone}</li>
+                <li>이메일: {email}</li>
+            </ul>
         </div>
     )
 }
